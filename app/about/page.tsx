6ba4a9ab0944bec2585e68fd51d1f0ca6afcaf22
jay suthar <jay37192@gmail.com>
2024-05-01 10:52:45 +0530

    #

---
+++ app/about/page.tsx
@@ -0,0 +1,48 @@
+import Image from "next/image";
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="flexCenter flex-col">
+      <div className="padding-container max-container w-full pb-24">
+        <Image src="/camp.svg" alt="camp" width={50} height={50} />
+        <p className="uppercase regular-18 -mt-1 mb-3 text-green-50">
+          About US
+        </p>
+        <div className="">
+          <div className="flex flex-wrap justify-between gap-5 lg:gap-10 pt-8">
+            <p className="regular-16 text-gray-30 xl:max-w-[520px]">
+              <h2 className="bold-40 lg:bold-64 xl:max-w-[390px] text-black mb-4">
+                Flint, Tinder & Spark!
+              </h2>
+              'Machan this is awesome! Why don't we offer this experience to
+              others?' We were camping in a vineyard somewhere near a town
+              called Suruli in Theni district of Tamilnadu. The conversation we
+              had that night was the spark. We had managed to import a camping
+              tent through a friend who had gone on an onsite to the US.
+              Suddenly our lives changed! We got access to breathtaking
+              locations.That’s how we had ended up in the vineyard.
+            </p>
+          </div>
+          <div className="flex flex-wrap justify-between gap-5 lg:gap-10">
+            <p className="regular-16 text-gray-30 xl:max-w-[520px]">
+              <h2 className="bold-40 lg:bold-64 text-black xl:max-w-[450px] pt-8 mb-4">
+                The Journey:
+              </h2>
+              Biggest concerns for anyone wanting to go camping in India are
+              finding safe campsites & clean toilets. And so we set out with the
+              sole motto of addressing these painpoints. We shaped & reshaped
+              the idea over several camps, worked on infra, safety & got it into
+              a workable form.Today as we speak, all camspites listed in
+              Exoticamp are privately owned campsites, with clean toilets, basic
+              first-aid kit, trained local hosts & back-up cottage / dormitories
+              for rough weather contingency.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
--- components/Modal.tsx
+++ components/Modal.tsx
@@ -5,13 +5,11 @@ import React from "react";
 const Modal = () => {
   return (
     <div className="absolute top-[5rem] right-2 bg-green-90 h-[15rem] text-white w-[15rem] rounded-xl">
-      <ul className="text-center pt-4">
+      <ul className="text-center pt-3">
         {NAV_LINKS.map((link) => (
           <>
             <div key={link.key} className="pt-4">
-              <Link href={link.href} className="">
-                {link.label}
-              </Link>
+              <Link href={link.href}>{link.label}</Link>
               <br />
             </div>
           </>
